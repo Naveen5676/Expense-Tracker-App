93e0fea367d632977482f1f866bebc5a32c82529
Naveen5676 <sample.first@yahoo.com>
2023-12-03 23:26:53 +0530

    Login Screen

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-
-import './App.css';
-import { Fragment } from 'react';
-import Signuppage from './Components/Signuppage';
+import "./App.css";
+import { Fragment } from "react";
+import Signuppage from "./Components/Signuppage";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <Fragment>
-      <Signuppage/>
+      <Switch>
+      <Route path="/" exact>
+          <Redirect to="/login" />
+        </Route>
+        <Route path="/login">
+          <Signuppage />
+        </Route>
+        <Route path="/home">
+          <Home />
+        </Route>
+      </Switch>
     </Fragment>
   );
 }
--- src/Components/Signuppage.js
+++ src/Components/Signuppage.js
@@ -1,22 +1,33 @@
 import Button from "react-bootstrap/Button";
-import React, { Fragment, useRef } from "react";
+import React, { Fragment, useRef, useState } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import {useHistory} from 'react-router-dom'
 
 const Signuppage = () => {
   const enetredemail = useRef();
   const enetredPwd = useRef();
   const renenterdpwd = useRef();
+  const [isLogin, setIsLogin] = useState(true);
+  const history = useHistory();
+
+  const onClickHandler=()=>{
+    setIsLogin((prevvalue)=>!prevvalue);
+  }
 
   const submitHandler = (e) => {
     e.preventDefault();
     const email = enetredemail.current.value;
     const pwd = enetredPwd.current.value;
-    const renterdpwd = renenterdpwd.current.value;
+    let renterdpwd; // Declare renterdpwd outside the block
 
-    if (pwd === renterdpwd) {
+  if (!isLogin) {
+    renterdpwd = renenterdpwd.current.value; // Assign value inside the block
+  }
+
+    if (isLogin) {
       fetch(
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA20QgzIbGGBJE2GjAckzUje0TsQ023o2M",
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA20QgzIbGGBJE2GjAckzUje0TsQ023o2M",
         {
           method: "POST",
           body: JSON.stringify({
@@ -24,37 +35,73 @@ const Signuppage = () => {
             password: pwd,
             returnSecureToken: true,
           }),
-          headers: {
-            "Content-Type": "application/json",
-          },
+          headers: { "Content-Type": "application/json" },
         }
       )
         .then((res) => {
           if (res.ok) {
-            alert("account created successfully");
+            alert("logged in successfully");
+            history.replace('/home');
           } else {
-            throw new Error("could not create account");
+            throw new Error("error");
           }
         })
         .catch((err) => {
-          console.error(err);
+          alert(err);
         });
     } else {
-      alert("password are not same");
+      if (pwd === renterdpwd) {
+        fetch(
+          "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA20QgzIbGGBJE2GjAckzUje0TsQ023o2M",
+          {
+            method: "POST",
+            body: JSON.stringify({
+              email: email,
+              password: pwd,
+              returnSecureToken: true,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+          .then((res) => {
+            if (res.ok) {
+              alert("account created successfully");
+            } else {
+              throw new Error("could not create account");
+            }
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+      } else {
+        alert("password are not same");
+      }
     }
   };
   return (
     <Fragment>
       <Container
-        className="d-flex  align-items-center justify-content-center"
+        className="d-flex align-items-center justify-content-center"
         style={{ minHeight: "100vh" }}
       >
-        {/* The d-flex class makes the container a flex container, align-items-center centers items vertically, and justify-content-center centers items horizontally. The minHeight: "100vh" ensures the container takes at least the full height of the viewport. */}
         <Row>
           <Col xs={18} md={20} xl={14}>
-            {/* xs={12} makes the column take full width on small screens, and md={6} makes it take half width on medium and larger screens. */}
-            <Card style={{ backgroundColor: "gray" }}>
-              <Card.Body>
+            <Card
+              style={{
+                backgroundColor: "gray",
+                width: "40rem",
+                height: "21rem",
+              }}
+            >
+              <Card.Title
+                className="text-center mt-1"
+                style={{ color: "white", fontSize: "30px", fontWeight: "bold" }}
+              >
+               {isLogin ? 'login':'Sign Up'}
+              </Card.Title>
+              <Card.Body style={{ fontSize: "25px" }}>
                 <form onSubmit={submitHandler}>
                   <label>Email</label>
                   <input
@@ -63,7 +110,7 @@ const Signuppage = () => {
                     className="form-control mb-2"
                     ref={enetredemail}
                   ></input>
-                  {/* The form-control class styles the input as a block-level element with full width. mb-2 adds margin-bottom for spacing. */}
+
                   <label>Password</label>
                   <input
                     type="password"
@@ -71,20 +118,21 @@ const Signuppage = () => {
                     className="form-control mb-2"
                     ref={enetredPwd}
                   ></input>
-                  {/* <label>Re-enter Password</label> */}
-                  <input
+
+                 {!isLogin &&<input
                     type="password"
                     placeholder="Re-enter Password"
                     className="form-control mb-3"
                     ref={renenterdpwd}
-                  ></input>
-                  {/* Added label for re-enter password and adjusted the spacing. */}
+                    {...(!isLogin ? { required: true } : {required: false})}
+                  ></input>}
+
                   <Button type="submit" variant="primary" block>
-                    Sign Up
+                  {isLogin ? "Login" : "create Account"}
                   </Button>
-                  {/* The block prop makes the button a block-level element, taking full width of the parent. */}
                 </form>
               </Card.Body>
+              <Button onClick={onClickHandler} className="mt-3" variant="dark">{isLogin ? 'Dont Have an account? Click Here': 'Have an account? click here to login'} </Button>
             </Card>
           </Col>
         </Row>
